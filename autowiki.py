abdf20f1e9996a425f97adc5f1898d3466a969b2
iahmed20 <103712279+iahmed20@users.noreply.github.com>
2025-06-02 22:34:00 -0500

    Update autowiki.py

--- autowiki.py
+++ autowiki.py
@@ -136,7 +136,7 @@ def retrieve_and_generate(topic, sections):
     tokenizer.pad_token = tokenizer.eos_token
 
 
-    # Initialize the pipeline with the loaded model and tokenizer
+
     pipe = pipeline(
         "text-generation",
         model=model,
@@ -145,10 +145,10 @@ def retrieve_and_generate(topic, sections):
         max_new_tokens=500,
         return_full_text=False,
         #repeat_penalty = 1.4 #cause issues
-        # Increased max_new_tokens for more substantial text
+
     )
 
-    # Wrap the pipeline in HuggingFacePipeline
+
     llm = HuggingFacePipeline(pipeline=pipe)
 
     article = f"# {topic}\n\n"
